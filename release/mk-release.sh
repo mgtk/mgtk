cc8e67d57b07b19d84daaf71fd08a6ca99387e17
Henning Niss <henningniss@gmail.com>
2000-06-09 08:23:17 +0000

    Added more options.

--- release/mk-release.sh
+++ release/mk-release.sh
@@ -1,11 +1,73 @@
 #!/bin/sh
 
-if [ "x$1" = "x" ]; then
-    echo "Usage: ./mk-release.sh <release name>"
-    exit 1
+Usage () {
+    exec cat << END_USAGE
+Usage: ./mk-release.sh <options>
+
+  Where options can be:
+  -type <release type> ('alpha' or 'beta', for example)
+  -version <version> ('0.02', for example)
+  -release <suffix> ('pre2', for example)
+  -o <filename> the output file (if omitted the filename is constructed
+                     as 'mgtk<-release type>-<version><release>.tar.gz')
+  -clean remove the directory constructed during generation
+
+  Only -version is required.
+END_USAGE
+}
+
+RELTYPE="alpha"
+VERSION=""
+RELEASE=""
+CLEAN=""
+
+while [ "$#" != "0" ]; do
+    arg=$1; shift
+    case $arg in
+      -type)
+         RELTYPE="$1"; TYPE="-$RELTYPE"; shift
+         ;;
+      -version)
+         VERSION=$1; shift
+         ;;
+      -release)
+         RELEASE=$1; shift
+         ;;
+      -o)
+         TARFILE=$1; shift
+	 ;;
+      -clean)
+         CLEAN="true"
+	 ;;
+      *)
+	 Usage
+         exit 1
+         ;;
+    esac
+done
+
+if [ "x$VERSION" = "x" ]; then
+    echo "No version specified!\n"
+    Usage
 fi
 
-# Configuration
+if [ "x$TARFILE" = "x" ]; then
+    TARFILE="mgtk$TYPE-$VERSION$RELEASE.tar"
+else
+    case $TARFILE in
+      *\.tar\.gz)
+         TARFILE=`echo $TARFILE | sed 's/\.tar\.gz/.tar/'`
+	 ;;
+      *\.tar)
+	 ;;
+      *)
+         TARFILE="$TARFILE.tar"
+	 ;;
+    esac
+fi
+
+echo "Generating mGTK $RELTYPE release v. $VERSION$RELEASE."
+echo "(dir= mgtk$TYPE; tar= $TARFILE)\n"
 
 # ... directories
 MGTKHOME=$HOME/mgtk
@@ -13,15 +75,12 @@ EXAMPLES=$MGTKHOME/examples
 MOSIDE=$EXAMPLES/moside
 DOCS=$MGTKHOME/docs
 SRC=$MGTKHOME/src
-TARFILE=$1.tar
+TRGT=mgtk$TYPE
 
 cd $MGTKHOME/release
 
-echo Generating release $1
-echo
-
-if [ -d mgtk-alpha ]; then
-    echo "The directory mgtk-alpha already exists!"
+if [ -d $TRGT ]; then
+    echo "The directory $TRGT already exists!"
     echo "Rename and restart."
     exit 1
 fi
@@ -33,49 +92,54 @@ if [ -f $TARFILE.gz ]; then
 fi
 
 echo Making directory structure ...
-mkdir mgtk-alpha
-mkdir mgtk-alpha/examples
-mkdir mgtk-alpha/examples/moside
-mkdir mgtk-alpha/docs
+mkdir $TRGT
+mkdir $TRGT/examples
+mkdir $TRGT/examples/moside
+mkdir $TRGT/docs
 
 echo Copying release specific files ...
-cp Make* mgtk-alpha
-cp INSTALL mgtk-alpha
+cp Make* $TRGT
+cp INSTALL $TRGT
 
 echo Copying global files ...
-cp $MGTKHOME/ANNOUNCEMENT mgtk-alpha
-cp $MGTKHOME/CONTRIB mgtk-alpha
-cp $MGTKHOME/Changes mgtk-alpha
-cp $MGTKHOME/LICENSE mgtk-alpha
-cp $MGTKHOME/README mgtk-alpha
-cp $DOCS/README mgtk-alpha/docs
+cp $MGTKHOME/ANNOUNCEMENT $TRGT
+cp $MGTKHOME/CONTRIB $TRGT
+cp $MGTKHOME/Changes $TRGT
+cp $MGTKHOME/LICENSE $TRGT
+cp $MGTKHOME/README $TRGT
+cp $DOCS/README $TRGT/docs
 
 echo Copying core files ...
-cp $SRC/Gtk.sig mgtk-alpha
-cp $SRC/Gtk.sml mgtk-alpha
-cp $SRC/mgtk.c mgtk-alpha
+cp $SRC/Gtk.sig $TRGT
+cp $SRC/Gtk.sml $TRGT
+cp $SRC/mgtk.c $TRGT
 
 echo Copying examples ...
-cp $EXAMPLES/README mgtk-alpha/examples
-cp $EXAMPLES/Makefile.w32 mgtk-alpha/examples
-cp $EXAMPLES/*.sig mgtk-alpha/examples
-cp $EXAMPLES/*.sml mgtk-alpha/examples
+cp $EXAMPLES/README $TRGT/examples
+cp $EXAMPLES/Makefile.w32 $TRGT/examples
+cp $EXAMPLES/*.sig $TRGT/examples
+cp $EXAMPLES/*.sml $TRGT/examples
 #   We have to treat the Makefile specially since it
 #   refers to the ../src/Makefile.inc
-#   Wont do: cp $EXAMPLES/Makefile mgtk-alpha/examples
-sed 's/^include.*Makefile\.inc$/include ..\/Makefile.inc/' $EXAMPLES/Makefile > mgtk-alpha/examples/Makefile
+#   Wont do: cp $EXAMPLES/Makefile $TRGT/examples
+sed 's/^include.*Makefile\.inc$/include ..\/Makefile.inc/' $EXAMPLES/Makefile > $TRGT/examples/Makefile
 
 echo Copying mosIDE ...
-cp $MOSIDE/README mgtk-alpha/examples/moside
-cp $MOSIDE/*.sig mgtk-alpha/examples/moside
-cp $MOSIDE/*.sml mgtk-alpha/examples/moside
-cp $MOSIDE/*.c mgtk-alpha/examples/moside
+cp $MOSIDE/README $TRGT/examples/moside
+cp $MOSIDE/*.sig $TRGT/examples/moside
+cp $MOSIDE/*.sml $TRGT/examples/moside
+cp $MOSIDE/*.c $TRGT/examples/moside
 #   We have to treat the Makefile specially since it
 #   refers to the ../src/Makefile.inc
-#   Wont do: cp $MOSIDE/Makefile mgtk-alpha/examples/moside
-sed 's/^include.*Makefile\.inc$/include ..\/..\/Makefile.inc/' $MOSIDE/Makefile > mgtk-alpha/examples/moside/Makefile
+#   Wont do: cp $MOSIDE/Makefile $TRGT/examples/moside
+sed 's/^include.*Makefile\.inc$/include ..\/..\/Makefile.inc/' $MOSIDE/Makefile > $TRGT/examples/moside/Makefile
 
 
 echo Constructing .tar.gz file ...
-tar cf $TARFILE mgtk-alpha/
+tar cf $TARFILE $TRGT/
 gzip $TARFILE
+
+if [ "$CLEAN" = "true" ]; then
+    echo Removing directory $TRGT
+    rm -rf $TRGT
+fi
