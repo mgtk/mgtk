17f7a65864b016a22c884ca3cf748645cf715e52
Henning Niss <henningniss@gmail.com>
2000-07-20 14:16:06 +0000

    Switched to mosmlpm.

---
+++ src/mgtkgen/defs2sml.pm
@@ -0,0 +1,18 @@
+structure Parsercomb.sml
+structure ArgParse.sml
+structure WSeq.sml
+
+structure Util.sml
+structure NameUtil.sml
+structure State.sml
+structure Messages.sml
+
+structure TypeExp.sml
+structure TypeInfo.sml
+structure AST.sml
+structure ParseUtils.sml
+structure Parser.sml
+structure Parse.sml
+structure Translate.sml
+
+structure Main.sml
