a01c2ace6c81f729be59f67b02fc626394fe6965
Henning Niss <henningniss@gmail.com>
2004-09-28 21:09:36 +0000

    Oops.

--- src/defs2sml/lib/preamble.c
+++ src/defs2sml/lib/preamble.c
@@ -216,18 +216,17 @@ EXTERNML value mgtk_get_null(value dummy) { /* ML */
    abtract values.
 */
 
-#define GValue_val(arg) ( (GValue*) Field(arg, 0) )
-#define GValue_val_nocast(arg) ( Field(arg, 0) )
+#define GValue_val(arg) ( (GValue*) Field(arg, 1) )
+#define GValue_val_nocast(arg) ( Field(arg, 1) )
 
 static void ml_finalize_gvalue (value val) {
-  g_value_unset ((GValue*) &Field(val,1)); 
+  g_value_unset ((GValue*) Field(val,1)); 
 }
 
 static inline value create_GValue (GType type) { 
   value res; 
-  res = alloc_final (2, ml_finalize_gvalue, 0, 1);
-  GValue_val_nocast(res) = (value) malloc(sizeof(GValue));
-  memset(GValue_val(res), 0, sizeof(GValue));
+  res = alloc_final (3 + 1, ml_finalize_gvalue, 0, 1);
+  *GValue(res) = {0, };
   g_value_init(GValue_val(res), type);
   return res; 
 }
